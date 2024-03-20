4d29d0cae6d5af900d9c985a53118194bb5488fb
ravikant <Ravikantwaghmare@vionsys.com>
2024-03-20 16:59:32 +0530

    cloud config changes

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 const fs = require('fs');
 
 cloudinary.config({ 
-    cloud_name: 'dugtxvaxh', 
-    api_key: '988148827526966', 
-    api_secret: 'HnnQjR2ATv0A8QyOim5patEwdHg' 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_CLOUD_KEY, 
+    api_secret: process.env.CLOUDINARY_CLOUD_SECRET 
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
