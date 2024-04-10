fd748a777a2bdb19f04458fdb482977763e95dca
ravikant <Ravikantwaghmare@vionsys.com>
2024-04-10 18:17:42 +0530

    bugs resolved

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -4,11 +4,11 @@ const authController = require("../controller/authController");
 const taskController = require("../controller/taskController");
 
 //tasks routes
+router.get('/findAll/:id', authController.protect, taskController.getTasksFromUserId);
+router.get('/getAll', authController.protect, taskController.getAllTasks);
 router.post('/create', authController.protect, authController.restrictTo(["admin"]), taskController.createTask);
 router.patch('/started/:id', authController.protect, taskController.updateTaskStart);
 router.patch('/completed/:id', authController.protect, taskController.updateTaskCompleted);
-router.get('/getAll/:id', authController.protect, taskController.getTasksFromUserId);
-router.get('/getAll', authController.protect, taskController.getAllTasks);
 
 
 module.exports = router;
