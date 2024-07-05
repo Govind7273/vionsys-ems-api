fadc5aea58b3fe60004c7f5b771486d06017fd69
sagarvionsys <sagaryenkure@vionsys.com>
2024-07-05 10:54:12 +0530

    firebase service env added

--- utils/sendNotificationToAll.js
+++ utils/sendNotificationToAll.js
@@ -1,23 +1,32 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../assets/vionsys-ems-notify-firebase-adminsdk-e38f9-559ff75b99.json");
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-const sendNotificationtoAll = async (registrationTokens, values) => {
-  const { title, description } = values;
+const sendNotificationToAll = async (registrationTokens, values) => {
+  const { title, description, image } = values; // Accept image from server
   try {
     const message = {
-      tokens: registrationTokens, // Notice the change here
+      tokens: registrationTokens,
       notification: {
         title: title,
         body: description,
-      },
-      webpush: {
-        headers: {
-          image: "/assets/logo.png",
-        },
+        image: image, // Use the image from the server
       },
       android: {
         priority: "high",
@@ -36,10 +45,11 @@ const sendNotificationtoAll = async (registrationTokens, values) => {
     };
 
     await admin.messaging().sendEachForMulticast(message);
+    console.log("Notification sent successfully");
   } catch (error) {
-    console.log("recipients error - ", error);
-    throw new Error("error while sending message to recipients ");
+    console.error("Error sending notification:", error);
+    throw new Error("Error while sending message to recipients");
   }
 };
 
-module.exports = sendNotificationtoAll;
+module.exports = sendNotificationToAll;
