848046f70e147109f163bec3429a7a9d50bdb206
RamanPatil68 <Ramanpatil@vionsys.com>
2024-09-09 18:03:28 +0530

    test

--- controller/leaveController.js
+++ controller/leaveController.js
@@ -72,28 +72,34 @@ exports.createLeaveRequest = async (req, res) => {
     leaveStartDate.setDate(leaveStartDate.getDate() - 1);
     const leaveStartDay = leaveStartDate.getDate();
 
+    const leaveEndDate = new Date(leaveData.leaveEnd);
+    leaveEndDate.setDate(leaveEndDate.getDate() - 1);
+    const leaveEndDay = leaveEndDate.getDate();
+
     //  console.log("Current Day:", currentDay);
     //  console.log("Leave Start Day (after subtracting one day):", leaveStartDay);
     //  console.log("One Day Before:", oneDayBefore.getDate());
 
     // Check if the leave start day is equal to the previous day
-    if (leaveStartDay === oneDayBefore.getDate()) {
-      if (!leaveData?.isHalfDay) {
-        // Full-day leave condition
-        if (currentHour >= 10) {
-          throw new Error("Full day leave must be applied before 10 AM");
-        }
-      } else {
-        // Half-day leave condition
-        if (currentHour > 14) {
-          throw new Error("Half day leave must be applied before 1 PM");
-        }
-      }
-    }
+    // if (leaveStartDay === oneDayBefore.getDate()) {
+    //   if (!leaveData?.isHalfDay) {
+    //     // Full-day leave condition
+    //     if (currentHour >= 10) {
+    //       throw new Error("Full day leave must be applied before 10 AM");
+    //     }
+    //   } else {
+    //     // Half-day leave condition
+    //     if(leaveData?.isHalfDay === true){
+    //     if (currentHour > 14) {
+    //       throw new Error("Half day leave must be applied before 1 PM");
+    //     }
+    //   }
+    //   }
+    // }
 
     if (
-      new Date(leaveData?.leaveStart) < new Date().setHours(0, 0, 0, 0) ||
-      new Date(leaveData?.leaveEnd) < new Date().setHours(0, 0, 0, 0)
+      new Date(leaveData?.leaveStart).setHours(0, 0, 0, 0) < new Date().setHours(0, 0, 0, 0) ||
+      new Date(leaveData?.leaveEnd).setHours(0, 0, 0, 0) < new Date().setHours(0, 0, 0, 0)
     ) {
       throw new Error("Leave dates must be in the future");
     }
